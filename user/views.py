6e8af7b0c275e126e092e52474bd9ffafc40796c
huan00 <96999144+huan00@users.noreply.github.com>
2023-10-21 23:01:56 -0400

    change get env to os.environ

--- user/views.py
+++ user/views.py
@@ -191,8 +191,8 @@ def get_outfit(request):
 @api_view(['POST'])
 def get_my_outfit(request):
     try:
-        env = environ.Env()
-        environ.Env.read_env()
+        # env = environ.Env()
+        # environ.Env.read_env()
         openai.api_key = os.environ['OPENAI_API_KEY']
         print( os.environ['OPENAI_API_KEY'])
         # print(os.getenv('OPENAI_API_KEY'))
@@ -239,7 +239,7 @@ def get_my_outfit(request):
                 '''
                 
                 """
-        # return Response(prompt)
+        return Response(prompt)
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
